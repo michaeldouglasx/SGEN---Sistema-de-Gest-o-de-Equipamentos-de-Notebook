8e7f9a08dfc1ad80f85d0ae6e5b38ad5ac8a1f22
Michael Douglas Figueirôa dos Santos <06190346103@ibama.gov.br>
2026-03-10 11:10:19 -0300

    adição do admin-user

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+@admin.register(User)
+class UserPersonalizadoAdmin(UserAdmin):
+    list_display = ('fullname', 'email_academico', 'phone', )
+    search_fields = ('fullname', 'email_academico', 'phone',)
+    list_per_page = 30
+    fieldsets = UserAdmin.fieldsets + (('Informações Acadêmicas', {'fields': ('fullname', 'email_academico','phone')}),)
+    add_fieldsets = UserAdmin.add_fieldsets + (('Informações Acadêmicas', {'fields': ('fullname', 'email_academico','phone')}),)
+
--- accounts/models.py
+++ accounts/models.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     id = models.BigAutoField(primary_key=True, null=False)
-    fullname = models.CharField(max_length=150, blank=True)
+    fullname = models.CharField(max_length=150, blank=True, verbose_name="Nome Completo")
     email_pessoal = models.EmailField(max_length=150, blank=True)
-    email_academico = models.EmailField( max_length=150, blank=False)
+    email_academico = models.EmailField( max_length=150, blank=False, verbose_name="E-mail Acadêmico", help_text="Example: aluno@edu.df.senac.br")
     phone = models.CharField(blank=False, max_length= 15, help_text="Formato (61) 9 9888-7777", verbose_name="Telefone de contato" )
     
     def __str__(self):
