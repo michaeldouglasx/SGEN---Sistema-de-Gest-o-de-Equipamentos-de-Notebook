3e58b67476bbfa7b65ede296c0c924f33a0ecfec
michael <michael_douglasfs@hotmail.com>
2026-03-17 19:53:02 -0300

    acrescentando logout

--- accounts/urls/urls.py
+++ accounts/urls/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from ..views.LoginView import Login_View
 from ..views.CadastroView import Cadastro_View
 from ..views.ReservaView import Reserva_View
+from ..views.LogoutView import Logout_View
 
 
 urlpatterns = [
     path('login/', Login_View, name='login'),
     path('cadastro/', Cadastro_View, name='cadastro'),
-    path('Reserva/', Reserva_View, name='reserva')
+    path('reserva/', Reserva_View, name='reserva'),
+    path('logout/', Logout_View, name='logout')
 
 ]
--- accounts/views/LoginView.py
+++ accounts/views/LoginView.py
@@ -2,16 +2,21 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login
+
 def Login_View(request):
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
+
         if user is not None:
             login(request, user)
             return redirect('reserva')
-        else:   
+        else:
             login_form = AuthenticationForm()
+            return render(request,'login.html', {'login_form': login_form})
+            
     else:
         login_form = AuthenticationForm()
         return render(request,'login.html', {'login_form': login_form})
+    
